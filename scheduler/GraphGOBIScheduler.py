1ce24a1f356a85cea604798637ed147e1683ff8e
ImMohammadHosseini <sayedmh50@gmail.com>
2023-02-12 13:30:20 +0330

    develop first_steps method

--- scheduler/GraphGOBIScheduler.py
+++ scheduler/GraphGOBIScheduler.py
@@ -15,55 +15,76 @@ from .GraphBaGTI.train import load_model
 from .GraphBaGTI.src.graph_representation import GNNEncoder
 from .GraphBaGTI.src.models import energy_latency_50
 from .GraphBaGTI.src.utils import *
-from .GraphBaGTI.src.opt import opt
+from .GraphBaGTI.src.opt import first_step_opt, second_step_opt
 
 
 class GraphGOBIScheduler(Scheduler):
     def __init__ (self, data_type) :
         self.graphRepre = GNNEncoder ()
-
-        #TODO check inflects between two projects
         self.model = eval(data_type+"()")
-        self.model, _, _, _ = load_model(data_type, self.model, data_type)
         self.data_type = data_type
-        self.hosts = int(data_type.split('_')[-1])
+        self.hosts = int(data_type.split('_')[-2])
+        self.vms = int(data_type.split('_')[-1])
+        
+        
+        
+        #TODO check to add the rest
+        self.model, _, _, _ = load_model(data_type, self.model, data_type)
+        
         dtl = data_type.split('_')
         #_, _, self.max_container_ips = eval("load_"+'_'.join(dtl[:-1])+
         #                              "_data("+dtl[-1]+")")
         
-    def suitable_init (self, emb_host_x, emb_container_x) :
-        pass
-    
-    def run_GraphGOBI (self) :
-        pass
     
     def selection (self) :
         return []
     
-    def first_step_init (self, vm_emb, instance_emb) :
+    def first_step_init (self, vm_emb, instance_emb, inst_schedueled) :
         vm_emb = vm_emb.detach().numpy()
         instance_emb = instance_emb.detach().numpy()
+        schedueled_instances = schedueled_instances.detach().numpy()
         
-        suitabel_instance_init = np.zeros(vm_emb.shape)
-        graph_i_id = self.env.graph_data['instance'].id.numpy().reshape(-1)
+        inits = []
+        ins_ids_init = []; ins_id_init = []; ins_prep = []; oneHots = [];
+        for i, inst_emb in enumerate(instance_emb):
+            if i not in inst_schedueled:
+                ins_id_init.append(i)
+                ins_prep.append(inst_emb)
+                oneHot = [0] * len(vm_emb)
+                oneHot[np.random.randint(0,len(vm_emb))] = 1
+                oneHots.append(oneHot)
+                if len(ins_prep) == len(vm_emb):
+                    two_emb = np.concatenate((vm_emb, ins_prep), axis=1)
+                    inits.append(np.concatenate((two_emb, oneHots), axis=1))
+                    ins_ids_init.append(ins_id_init)
+                    ins_id_init=[]; ins_prep = []; oneHots = []
         
-        
-        for i, c in enumerate(self.env.containerlist):#TODO instancelist  id changable
-            if c:
-                idx = np.where(graph_c_id == c.id)[0][0]
-                suitabel_container_init[i] = emb_container_x[idx]
-        
-        return np.concatenate((emb_host_x, suitabel_container_init), axis=1)
+        return inits, ins_ids_init
     
     def first_step (self, first_sch = False) :
         if not first_sch: self.env.nodeUpdate()
+        schedueled_instances = self.env.graphData['instance', 'run_in', 'vm'].edge_index[0]
         _, vm_emb, instance_emb = self.graphRepre(self.env.graph_data)
-        
+        init, instance_ids_init = self.first_step_init(vm_emb, 
+                                                       instance_emb, 
+                                                       schedueled_instances)
+        init = torch.tensor(init, dtype=torch.float, requires_grad=True)
+        result, iteration, fitness = first_step_opt(init, 
+                                                    self.model, 
+                                                    self.data_type)
+        decision = []
+        #TODO add suitable decision
+        return decision
     
+    def first_step_init (self) :
+        pass
     def second_step (self) :
         pass
     
-    def placement (self, containerIDs, first_sch = False) :
-        self.first_step(first_sch)
-        self.second_step()
-        #return d1, d2
+    def instancePlacement (self, containerIDs, first_sch = False) :
+        d1 = self.first_step(first_sch)
+        d2 = self.second_step()
+        return d1, d2
+    
+    def vmPlacement (self):
+        pass
