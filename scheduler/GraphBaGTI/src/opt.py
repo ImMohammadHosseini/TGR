42f848c43b7c69b132271c9f53bfb3fa9434853f
ImMohammadHosseini <sayedmh50@gmail.com>
2023-02-13 12:28:14 +0330

    change opt method to use in two steps, fixed some bugs

--- scheduler/GraphBaGTI/src/opt.py
+++ scheduler/GraphBaGTI/src/opt.py
@@ -20,37 +20,38 @@ def convertToOneHot(dat, emb_part_old, nums):
     return new_dat_oneHot
 
 
-def first_step_opt (init, model, data_type):
-    VMS = int(data_type.split('_')[-1])
+def opt (init, model, num):
+    #VMS = int(data_type.split('_')[-1])
     optimizer = torch.optim.AdamW([init] , lr=0.8)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
     iteration = 0; equal = 0
     while iteration < 200:
-        emb_part_old = deepcopy(init.data[:,:,0:-VMS])
-        alloc_old = deepcopy(init.data[:,:,-VMS:])
+        emb_part_old = deepcopy(init.data[:,:,0:-num])
+        alloc_old = deepcopy(init.data[:,:,-num:])
         z = model(init)
         optimizer.zero_grad(); z.backward(); optimizer.step(); scheduler.step()
-        init.data = convertToOneHot(init.data, emb_part_old, VMS)
-        equal = equal + 1 if torch.all(alloc_old.eq(init.data[:,-VMS:])) else 0
+        init.data = convertToOneHot(init.data, emb_part_old, num)
+        equal = equal + 1 if torch.all(alloc_old.eq(init.data[:,:,-num:])) else 0
         if equal > 30: break
         iteration += 1
-        init.requires_grad = False 
-        return init.data, iteration, model(init)
+    init.requires_grad = False 
+    return init.data, iteration, model(init)
 
 
-
-def second_step_opt(init, model, bounds, data_type):
+'''
+def second_step_opt(init, model, data_type):
     HOSTS = int(data_type.split('_')[-2])
     optimizer = torch.optim.AdamW([init] , lr=0.8)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10)
     iteration = 0; equal = 0
     while iteration < 200:
-        cpu_old = deepcopy(init.data[:,0:-HOSTS]); alloc_old = deepcopy(init.data[:,-HOSTS:])
+        emb_part_old = deepcopy(init.data[:,:,0:-HOSTS]); 
+        alloc_old = deepcopy(init.data[:,:,-HOSTS:])
         z = model(init)
         optimizer.zero_grad(); z.backward(); optimizer.step(); scheduler.step()
-        init.data = convertToOneHot(init.data, cpu_old, HOSTS)
+        init.data = convertToOneHot(init.data, emb_part_old, HOSTS)
         equal = equal + 1 if torch.all(alloc_old.eq(init.data[:,-HOSTS:])) else 0
         if equal > 30: break
         iteration += 1
     init.requires_grad = False 
-    return init.data, iteration, model(init)
+    return init.data, iteration, model(init)'''
